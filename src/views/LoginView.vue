e2ddd586d5b7ff238fd22751dbe33e50c34e233a
Alissonn1325 <alissonfbrito01@gmail.com>
2024-07-10 16:52:07 -0300

    Login Quase Feito.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="Hh90wYMRWeC6JSeupGOWe1nk"
+    ></passage-auth>
+  </div>
+</template>
+
+<style></style>
